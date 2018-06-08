d4cad8d490cadd3ca505e22b88b059dedad119d5
penguin138 <fridaythethirteens@gmail.com>
2018-06-08 16:43:56 +0300

    fix names of entities from imported *.proto

--- src/purerpc/protoc_plugin/plugin.py
+++ src/purerpc/protoc_plugin/plugin.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import autopep8
+from collections import defaultdict
 
 from google.protobuf.compiler.plugin_pb2 import CodeGeneratorRequest
 from google.protobuf.compiler.plugin_pb2 import CodeGeneratorResponse
@@ -18,14 +19,29 @@ def get_python_package(proto_name):
     return package_name.replace("/", ".") + "_pb2"
 
 
+def simple_type(type_):
+    simple_type = type_.split(".")[-1]
+    return simple_type
+
+
 def get_python_type(proto_name, proto_type):
+    print(proto_name, proto_type, file=sys.stderr)
     if proto_type.startswith("."):
-        return get_python_package(proto_name) + proto_type
+        return get_python_package(proto_name) + "." + simple_type(proto_type)
     else:
         return proto_type
 
 
-def generate_single_proto(proto_file: descriptor_pb2.FileDescriptorProto):
+def get_proto(entity_name, imports, named_entities, current_proto):
+    simple_name = simple_type(entity_name)
+    for import_ in imports:
+        if simple_name in named_entities[import_]:
+            return import_
+    return current_proto
+
+
+def generate_single_proto(proto_file: descriptor_pb2.FileDescriptorProto,
+                          named_entities):
     contents = IMPORT_STRINGS
     contents += "import {}\n".format(get_python_package(proto_file.name))
     for dep_module in proto_file.dependency:
@@ -33,9 +49,8 @@ def generate_single_proto(proto_file: descriptor_pb2.FileDescriptorProto):
     for service in proto_file.service:
         contents += "\n\nclass {}Servicer(purerpc.server.Servicer):\n".format(service.name)
         for method in service.method:
-            contents += "    async def {}(self, input_message{}):\n".format(method.name,
-                                                                    "s" if
-                                                                    method.client_streaming else "")
+            contents += "    async def {}(self, input_message{}):\n".format(
+                method.name, "s" if method.client_streaming else "")
             contents += "        raise NotImplementedError()\n\n"
         contents += "    @property\n"
         contents += "    def service(self) -> purerpc.server.Service:\n"
@@ -45,9 +60,13 @@ def generate_single_proto(proto_file: descriptor_pb2.FileDescriptorProto):
                                                           response_stream=method.server_streaming)
             contents += ("        service_obj.add_method(\"{}\", self.{}, "
                          "RPCSignature({}, {}, {}))\n".format(
-                method.name, method.name, cardinality,
-                get_python_type(proto_file.name, method.input_type),
-                get_python_type(proto_file.name, method.output_type)))
+                             method.name, method.name, cardinality,
+                             get_python_type(get_proto(method.input_type, proto_file.dependency,
+                                                       named_entities, proto_file.name),
+                                             method.input_type),
+                             get_python_type(get_proto(method.output_type, proto_file.dependency,
+                                                       named_entities, proto_file.name),
+                                             method.output_type)))
         contents += "        return service_obj\n\n\n"
 
         contents += "class {}Stub:\n".format(service.name)
@@ -59,10 +78,13 @@ def generate_single_proto(proto_file: descriptor_pb2.FileDescriptorProto):
                                                           response_stream=method.server_streaming)
             contents += ("        self.{} = self._stub.get_method_stub("
                          "\"{}\", RPCSignature({}, {}, {}))\n".format(
-                method.name, method.name, cardinality,
-                get_python_type(proto_file.name, method.input_type),
-                get_python_type(proto_file.name, method.output_type)
-            ))
+                             method.name, method.name, cardinality,
+                             get_python_type(get_proto(method.input_type, proto_file.dependency,
+                                                       named_entities, proto_file.name),
+                                             method.input_type),
+                             get_python_type(get_proto(method.output_type, proto_file.dependency,
+                                                       named_entities, proto_file.name),
+                                             method.output_type)))
         contents += "\n\n"
 
     return autopep8.fix_code(contents, options={"experimental": True, "max_line_length": 100})
@@ -74,10 +96,20 @@ def main():
     files_to_generate = set(request.file_to_generate)
 
     response = CodeGeneratorResponse()
+    named_entities = defaultdict(set)
+    for proto_file in request.proto_file:
+        for message in proto_file.message_type:
+            named_entities[proto_file.name].add(message.name)
+        for enum in proto_file.enum_type:
+            named_entities[proto_file.name].add(enum.name)
+        for service in proto_file.service:
+            named_entities[proto_file.name].add(service.name)
+        for extension in proto_file.extension:
+            named_entities[proto_file.name].add(extension.name)
     for proto_file in request.proto_file:
         if proto_file.name in files_to_generate:
             out = response.file.add()
             out.name = proto_file.name.replace('.proto', "_grpc.py")
-            out.content = generate_single_proto(proto_file)
+            out.content = generate_single_proto(proto_file, named_entities)
 
     sys.stdout.buffer.write(response.SerializeToString())
