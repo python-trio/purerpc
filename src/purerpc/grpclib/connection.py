c6636f4ab1e9c7760d853f2ead77e4525f4d62a9
Andrew Stepanov <karn9050@gmail.com>
2019-02-15 23:16:20 +0300

    feat: support percent-encoded grpc-message header

--- src/purerpc/grpclib/connection.py
+++ src/purerpc/grpclib/connection.py
@@ -1,3 +1,4 @@
+import urllib.parse
 import logging
 import datetime
 
@@ -275,8 +276,7 @@ class GRPCConnection:
             ("grpc-status", str(status.int_value)),
         ]
         if status.status_message:
-            # TODO: should be percent encoded
-            trailers.append(("grpc-message", status.status_message))
+            trailers.append(("grpc-message", urllib.parse.quote(status.status_message, safe='')))
         trailers.extend(sanitize_headers(custom_metadata))
         self.h2_connection.send_headers(stream_id, trailers, end_stream=True)
 
@@ -285,7 +285,6 @@ class GRPCConnection:
             ("grpc-status", str(status.int_value)),
         ]
         if status.status_message:
-            # TODO: should be percent encoded
-            trailers.append(("grpc-message", status.status_message))
+            trailers.append(("grpc-message", urllib.parse.quote(status.status_message, safe='')))
         trailers.extend(sanitize_headers(custom_metadata))
         self.h2_connection.send_headers(stream_id, trailers, end_stream=True)
