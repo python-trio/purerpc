4d2a08a45358785c71e0b208d26deaf75573e86e
John Belmonte <john@neggie.net>
2022-04-17 19:50:09 +0900

    remove release notes from long description

--- setup.py
+++ setup.py
@@ -18,9 +18,8 @@ def main():
         license="Apache License Version 2.0",
         description=("Native, async Python gRPC client and server implementation "
                      "supporting asyncio, uvloop, trio"),
-        long_description='%s\n%s' % (
-            re.compile('^.. start-badges.*^.. end-badges', re.M | re.S).sub('', read('README.md')),
-            re.sub(':[a-z]+:`~?(.*?)`', r'``\1``', read('RELEASE.md'))
+        long_description=(
+            re.compile('^.. start-badges.*^.. end-badges', re.M | re.S).sub('', read('README.md'))
         ),
         long_description_content_type='text/markdown',
         author="Andrew Stepanov",
